1ee7daddacc64599fecd527c8a839e59b7cfdeec
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2020-09-07 22:14:54 -0400

    Adds active storage to handle product image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
   has_one_attached :image
 
+  # The following is needed to support deletion
+  attribute :remove_image, :boolean
+  after_save -> { image.purge }, if: :remove_image
+
   rails_admin do
+    field :name, :string
+    field :price, :float
+    field :description, :string
     field :image, :active_storage
   end
 end
